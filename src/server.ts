846b949e455989baedcbb2115053766e159ec891
Cezar Augusto Crummenauer <crummenauerca@gmail.com>
2021-02-23 20:37:45 -0300

    Creating route and controller of the users

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,12 @@
 import express from 'express'
 import 'reflect-metadata'
 import './database'
+import { router } from './routes'
 
 const app = express()
 
-app.get('/', (request, response) => {
-    return response.json({message: 'Hello NLW04'})
-})
-
 app.use(express.json())
-
-app.post('/', (request, response) => {
-    console.log(request.body)
-    return response.json({message: 'The data was received successfully'})
-})
+app.use(router)
 
 app.listen(3333, () => {
     console.log('Server is running!')
