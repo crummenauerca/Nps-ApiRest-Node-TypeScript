d69e58591e2bd1984b3e6e2283b7a707cb2601c3
Cezar Augusto Crummenauer <crummenauerca@gmail.com>
2021-02-23 03:08:37 -0300

    Getting a HTML content from server

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,10 @@ import express from 'express'
 
 const app = express()
 
+app.get('/users', (request, response) => {
+    return response.send("<h1>Hello NLW04</h1>")
+})
+
 app.listen(3333, () => {
     console.log('Server is running!')
 })
