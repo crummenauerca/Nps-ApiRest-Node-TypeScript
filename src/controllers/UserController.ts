4ef7106f1b97f0015acf97c3a2bfcbff6e3ec977
Cezar Augusto Crummenauer <crummenauerca@gmail.com>
2021-03-01 05:23:47 -0300

    Validating the users creation with Yup

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,10 +1,23 @@
 import { Request, Response } from 'express'
 import { getCustomRepository } from 'typeorm'
 import { UserRepository } from '../repositories/UserRepository'
+import * as yup from 'yup'
 
 class UserController {
   async create(request: Request, response: Response) {
     const { name, email } = request.body
+
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      email: yup.string().email().required(),
+    })
+
+    try {
+      await schema.validate(request.body, { abortEarly: false })
+    } catch (error) {
+      return response.status(400).json(error)
+    }
+
     const userRepository = getCustomRepository(UserRepository)
     const userAlreadyExists = await userRepository.findOne({ email })
 
