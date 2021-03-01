355700eb17ac37fdf14bc189ccb723d966c7af30
Cezar Augusto Crummenauer <crummenauerca@gmail.com>
2021-03-01 03:05:53 -0300

    Allow the user to complete the survey in the email

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -29,18 +29,19 @@ class SendMailController {
 
     const npsPath = resolve(__dirname, "..", "views", "emails", "npsMail.hbs")
 
-    const variables = {
-      name: user.name, title: survey.title,
-      description: survey.description, user_id: user.id,
-      link: process.env.URL_MAIL
-    }
-
     const surveyUserAlreadyExists = await surveyUserRepository.findOne({
-      where: [{ user_id: user.id }, { value: null }],
+      where: { user_id: user.id, value: null },
       relations: ["user", "survey"],
     })
 
+    const variables = {
+      name: user.name, title: survey.title,
+      description: survey.description, id: "",
+      link: process.env.URL_MAIL
+    }
+
     if (surveyUserAlreadyExists) {
+      variables.id = surveyUserAlreadyExists.id
       await SendMailService.execute(email, survey.title, variables, npsPath)
       return response.json(surveyUserAlreadyExists)
     }
@@ -50,6 +51,8 @@ class SendMailController {
       survey_id: survey.id
     })
     await surveyUserRepository.save(surveyUser)
+
+    variables.id = surveyUser.id
 
     await SendMailService.execute(user.email, survey.title, variables, npsPath)
 
